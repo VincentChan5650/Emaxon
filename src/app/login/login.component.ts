c1608b2880fe5c43a7587c054e4e800d257e910b
VincentChan5650 <vin9305@gmail.com>
2017-10-05 21:50:00 -0700

    add auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import {AngularFireAuth} from 'angularfire2/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+  ) { }
 
-  ngOnInit() {
+  login(){
+    this.auth.login();
   }
+  
 
 }
